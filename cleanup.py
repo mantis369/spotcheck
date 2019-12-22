efbb29f72d9dbf669828334202d70d6c0dad7cd2
Mantis <me@derikstiller.com>
2019-12-22 01:23:48 -0500

    Clean up cleanup script!
    1. Also remove DS_Store junk files from macOS.
    2. No more album whitelist.

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,4 @@
-from os import listdir, unlink
+from os import listdir, unlink, walk
 from os.path import isdir, join
 from shutil import rmtree
 
@@ -7,17 +7,8 @@ local_lib = "c:\\Users\\Derik Stiller\\Music"
 
 with open(infn, "r") as inf:
 	tracks = [ln.strip() for ln in inf]
-	albums = []
-	for t in tracks:
-		last_slash = t.rfind("\\")
-		a = t[:last_slash]
-		if not a in albums:
-			albums.append(a)
-		
-	print("We have", len(albums), "keeper albums with", len(tracks), "tracks.")
 	
-	del_count = 0
-	
+	del_count = 0	
 	for a in [a for a in listdir(local_lib) if not "desktop.ini" in a and not "MusicBee" in a]:
 		for al in [al for al in listdir(join(local_lib, a)) if isdir(join(local_lib, a)) and not "DS_Store" in al]:	
 			audio_files = [t for t in listdir(join(local_lib, a, al)) if not "DS_Store" in t and t.endswith(".mp3") or t.endswith(".m4a")]
@@ -35,10 +26,7 @@ with open(infn, "r") as inf:
 							print("\t\tGot rid of", f, "as a directory.")
 						except:
 							print("\t\tWasn't able to treat", f, "as a directory, either!")
-						
-			if join(local_lib, a, al) in albums:
-				continue
-				
+								
 			for t in audio_files:
 				fn = join(local_lib, a, al, t)
 				try:
@@ -65,12 +53,23 @@ def removeEmptyFolders(path, removeRoot=True):
 	
 	global empty_count	
 	files = listdir(path)
-	if len(files) == 0 and removeRoot:
-		empty_count += 1
+	if len(files) == 0 and removeRoot:		
 		try:
 			rmtree(path) #changed from os.rmdir for consistency with my code above
+			empty_count += 1
 		except:
 			print("Couldn't delete the directory", path)
+		
+junk_count = 0		
+for root, dirs, files in walk(local_lib, topdown=True):
+	for f in files:
+		if "DS_Store" in f:			
+			try:
+				unlink(join(root, f))
+				junk_count += 1
+			except:
+				print("Couldn't remove the junk file", join(root, f))
+print("I deleted", junk_count, "junk files.")
 		
 removeEmptyFolders(local_lib)
 print("I removed", empty_count, "empty directories.")
